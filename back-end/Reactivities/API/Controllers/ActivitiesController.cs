1151003fea1e29c90b9034b5c57f875cfccc79ac
Reza Nouri <reza.nouri1993@outlook.com>
2022-12-14 21:45:50 +0330

    add controller

---
+++ back-end/Reactivities/API/Controllers/ActivitiesController.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _dataContext;
+
+        public ActivitiesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetAtivities()
+        {
+            return await _dataContext.Activities.ToListAsync();
+        }
+    }
+}
