a23471e12311849a3e011742af19db38089fd9c3
Reza Nouri <reza.nouri1993@outlook.com>
2022-12-15 19:16:01 +0330

    complet crud

--- back-end/Reactivities/API/Controllers/ActivitiesController.cs
+++ back-end/Reactivities/API/Controllers/ActivitiesController.cs
@@ -21,5 +21,20 @@ namespace API.Controllers
         {
             return await Mediator.Send(new Details.Query { Id = id });
         }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateActivity(Activity activity)
+        {
+            activity.Id = Guid.NewGuid();
+            activity.Date = DateTime.Now;
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+        }
     }
 }
---
+++ back-end/Reactivities/Application/Activities/Edit.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                _context.Update(request.Activity);
+                await _context.SaveChangesAsync();
+                return Unit.Value;
+            }
+        }
+    }
+}
